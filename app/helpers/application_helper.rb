52c87395f966b065982b8b69821cd1f56727b603
Skyridge Devcamp <skyridge@devcamp.com>
2017-07-25 17:58:52 +0000

    implemented authorization for poerfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
     def login_helper
-        if current_user.is_a?(User)
+       if current_user.is_a?(GuestUser)
+             (link_to "Sign Up", new_user_registration_path) +
+        "<br>".html_safe +
+        (link_to "Login", new_user_session_path)
+       else
         (link_to "Logout", destroy_user_session_path, method: :delete) +
          "<br>".html_safe +
         (link_to "Edit User", edit_user_registration_path)
-        else
-        (link_to "Sign Up", new_user_registration_path) +
-        "<br>".html_safe +
-        (link_to "Login", new_user_session_path)
-        end
+       end
     end
     
     def source_helper(layout_name)
@@ -17,4 +17,4 @@ module ApplicationHelper
        content_tag(:p, greeting, class: "source-greeting")
         end 
     end
-end
+end    
