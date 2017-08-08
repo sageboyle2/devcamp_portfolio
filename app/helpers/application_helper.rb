b7acc105ebb7677ee3ec3332238f2207bbaff3fd
Skyridge Devcamp <skyridge@devcamp.com>
2017-08-08 00:12:36 +0000

    refacotred alerts into helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,25 @@
 module ApplicationHelper
-    def login_helper style = ''
-       if current_user.is_a?(GuestUser)
-             (link_to "Sign Up", new_user_registration_path, class: style) +
-        " ".html_safe +
-        (link_to "Login", new_user_session_path, class: style)
-       else
-        (link_to "My Account", edit_user_registration_path, class: style) +
-         " ".html_safe +
-        (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
-       end
+  def login_helper style = ''
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
-    
-    def source_helper(layout_name)
-        if session[:source] 
-            greeting = "Thanks for visiting me from #{session[:source]}. You are on the #{layout_name} layout"
-       content_tag(:p, greeting, class: "source-greeting")
-        end 
+  end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
     end
-    
+  end
+
+  def copyright_generator
+    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+  end
+
   def nav_items
     [
       {
@@ -42,18 +44,31 @@ module ApplicationHelper
       },
     ]
   end
-    
-    def nav_helper style, tag_type
-        nav_links = ''
-        
-        nav_items.each do |item|
-          nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
-        nav_links.html_safe
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
     end
-    
-    def active? path
-      "active" if current_page? path
-    end
-end    
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Sage Mercedes", sticky: false)
+  end
+
+end
