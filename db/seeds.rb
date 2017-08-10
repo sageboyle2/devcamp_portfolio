2042d067edbb474a76eb0dd15f5f976c921a734d
Skyridge Devcamp <skyridge@devcamp.com>
2017-08-10 17:01:20 +0000

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+    email: "sageboyle2@gmail.com",
+    password: "hannah18",
+    password_confirmation: "hannah18",
+    name: "Sage",
+    roles: "site_admin"
+    )
+
+puts "1 Admin user created"
+
+User.create!(
+    email: "test@test.com",
+    password: "asdfasdf",
+    password_confirmation: "asdfasdf",
+    name: "Regular User",
+    )
+
+puts "1 user created"
+
 3.times do |topic|
     Topic.create!(
         title: "Topic #{topic}"
